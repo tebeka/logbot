c14b5ed5e4d725778ebd1e3a813e1b473915ce52
Miki Tebeka <miki.tebeka@gmail.com>
2014-02-04 20:23:26 -0800

    presence notification
    --HG--
    branch : presence

---
+++ run-local.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Assumed that you have running openfire on localhost with password of 1234
+
+. ./venv/bin/activate
+
+host=$(hostname)
+
+python -m logbot \
+    --host ${host} \
+    --user logbot \
+    --passwd 1234 \
+    --no-tls \
+    biteam@conference.${host}
